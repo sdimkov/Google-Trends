c67e816cf513fa87cd67b69dbe8dc3a4107d26ca
Stoyan Dimkov <stoyan.dimkov@gmail.com>
2014-04-25 16:24:36 +0300

    Feature: Use a set of proxies and accounts.

--- google_trends.rb
+++ google_trends.rb
@@ -17,29 +17,58 @@ module GoogleTrends
   DEFAULT_SEARCH   = 'web'
   DEFAULT_OUTPUT   = nil
   DEFAULT_BREAK    = 0
+  DEFAULT_USE_PROXIES     = false
+  DEFAULT_CHANGE_ACCOUNTS = false
+  DEFAULT_CONNECTION_LIMIT = 800
+
+  PROXIES = [
+    "http://186.95.32.209:8080",
+    "http://107.170.93.27:3128",
+    "http://190.73.142.144:8080",
+    "http://190.78.26.232:8080",
+    "http://186.88.214.185:8080",
+    "http://200.8.139.47:8080"
+  ]
+
+  ACCOUNTS = [
+    [ 'username@gmail.com', 'password' ],
+    [ 'username@gmail.com', 'password' ],
+    [ 'username@gmail.com', 'password' ],
+    [ 'username@gmail.com', 'password' ],
+    [ 'username@gmail.com', 'password' ],
+    [ 'username@gmail.com', 'password' ],
+    [ 'username@gmail.com', 'password' ],
+  ]
 
 
   def self.console_application
+    
+    # Define variables and apply defaults
     options            = {}
-    username, password = nil, nil
-    input,    output   = nil, nil
-    pause,    count    = 0  , 0
+    username, password = DEFAULT_USERNAME, DEFAULT_PASSWORD
+    output,   input    = DEFAULT_OUTPUT, nil
+    pause,    count    = DEFAULT_BREAK, 0
+    use_proxies        = DEFAULT_USE_PROXIES
+    change_accounts    = DEFAULT_CHANGE_ACCOUNTS
+    connection_limit   = DEFAULT_CONNECTION_LIMIT
+    used_ips           = [ nil ]
+
+    options[:geo]   = DEFAULT_LOCATION
+    options[:cat]   = DEFAULT_CATEGORY
+    options[:gprop] = DEFAULT_SEARCH
 
     # Setup console interface
     optparse = OptionParser.new do |opts|
       opts.banner = "Usage google_trends.rb [options] keyword1 keyword2 ..."
-
-      options[:geo] = DEFAULT_LOCATION
+  
       opts.on( '-L', '--location AREA', 'Search in specific location') do |area|
         options[:geo] = area
       end
 
-      options[:cat] = DEFAULT_CATEGORY
       opts.on( '-C', '--category CATG', 'Search in specific category' ) do |catg|
         options[:cat] = catg
       end
 
-      options[:gprop] = DEFAULT_SEARCH
       opts.on( '-S', '--search TYPE', ['web', 'news', 'images', 'shopping', 'youtube'], 
           'Specify search type. Default is web',
           'Can be one of the following:',
@@ -51,12 +80,10 @@ module GoogleTrends
         options[:no_format] = true
       end
 
-      username = DEFAULT_USERNAME
       opts.on( '-U', '--username USER', 'Google account username/email') do |user|
         username = user
       end
 
-      password = DEFAULT_PASSWORD
       opts.on( '-P', '--password PASS', 'Google account password') do |pass|
         password = pass
       end
@@ -65,14 +92,29 @@ module GoogleTrends
         input = file
       end
 
-      output = DEFAULT_OUTPUT
       opts.on( '-O', '--output FILE', 'Save results from all queries in the specified file') do |file|
         output = file
       end
       
-      pause = DEFAULT_BREAK
       opts.on( '-B', '--break N', 'Pause execution of input queries after every N queries processed') do |n|
         pause = n.to_i
+      end
+
+      opts.on( '--use-proxies', 'Route the connection via proxies. Uses new proxy',
+                                'each time connection limit is reached') do
+        use_proxies = true
+      end
+
+      opts.on( '-A', '--change-accounts', 'Change user accounts each time connection limit is reached') do
+        change_accounts = true
+        if not username or not password
+          username, password = ACCOUNTS.pop
+        end
+      end
+
+      opts.on( '--connection-limit N', 'Change the connection after every N queries processed. Must be applied',
+                                       'in combination with --use-proxies or --change-accounts to have effect') do |n|
+        connection_limit = n.to_i
       end
 
       opts.on( '-H', '--help', 'Display this screen' ) do
@@ -114,6 +156,31 @@ module GoogleTrends
     unless queries.empty?
       client = Client.new username, password
       queries.each do |query|
+        if (use_proxies or change_accounts) and client.queries_count > connection_limit
+          puts "\nConnection limit reached!"
+          if change_accounts
+            if ACCOUNTS.empty?
+              puts 'No more accounts!'
+              exit
+            end
+            username, password = ACCOUNTS.pop
+            puts "Username changed to: " + username
+          end
+          new_client, new_proxy, ip = nil, nil, nil
+          if use_proxies
+            while used_ips.include? ip
+              if PROXIES.empty?
+                puts 'No more proxies!'
+                exit
+              end
+              new_proxy  = PROXIES.pop
+              new_client = HTTPClient.new new_proxy
+              ip = get_ip new_client
+            end
+            puts "Proxy changed to: " + new_proxy
+          end
+          client = Client.new username, password, new_client
+        end
         report = client.download_csv_report query
         count += 1
         if pause > 0 and count >= pause
@@ -136,13 +203,33 @@ module GoogleTrends
 
   end
 
+  def self.get_ip client
+    url = URI.parse 'http://wtfismyip.com'
+    headers = { 
+        'Referrer'     => 'https://www.google.com/accounts/ServiceLoginBoxAuth',
+        'User-Agent'   => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21',
+        'Content-type' => 'application/x-www-form-urlencoded',  
+        'Accept'       => 'text/html'
+    }
+    begin
+        return client.get(url, headers).body[/\d+\.\d+\.\d+\.\d+/] 
+    rescue Exception => e
+        return nil
+    end
+  end
+
+
   class Client
 
-    def initialize username, password
+    attr_reader :queries_count
+
+    def initialize username, password, http_client = nil
       unless username.is_a? String and password.is_a? String and 
              username.size > 1     and password.size > 1
         raise 'Provide valid username and password for Google authentication'
       end
+
+      @queries_count = 0
       
       @login_params = {
         'continue'         => 'http://www.google.com/trends',
@@ -162,7 +249,8 @@ module GoogleTrends
       @url_PerfCookie          = 'http://www.google.com'
       
       puts "\nLogin in Google Trends as #{username} ..."
-      @client = HTTPClient.new()
+      @client = http_client || HTTPClient.new 
+      @client.receive_timeout = 10000
       uri = URI.parse @url_ServiceLoginBoxAuth
       res = @client.get uri, @headers
 
@@ -189,6 +277,8 @@ module GoogleTrends
         'cat'     => nil,  # Category
         'gprop'   => nil,  # Search type
       }
+
+      @queries_count += 1
 
       params.keys.each do |key|
         params[key.to_s] = params.delete key unless key.is_a? String
